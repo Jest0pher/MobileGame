84056a5e100207e3f0fe1f5a2d1c2be092147efe
Jesse Flanary <JRFlanary@student.fullsail.edu>
2022-10-11 17:49:42 -0400

    Fix bug causing path lines to render before being scaled

--- MobileGame/Assets/ScreenScalar.cs
+++ MobileGame/Assets/ScreenScalar.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 public class ScreenScalar : MonoBehaviour
 {
     public Vector2 baseRes;
-    public Camera cam;
     // Start is called before the first frame update
-    void Update()
+    void Start()
     {
         transform.localScale = new Vector3(baseRes.y / Screen.height, baseRes.x / Screen.width, 1);
     }
